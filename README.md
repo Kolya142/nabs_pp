2b1321bba187ef4ea1762fb12e4df26f30a8a044
Kolya142 <87275865+Kolya142@users.noreply.github.com>
2024-11-22 14:13:42 +0300

    readme update & clear things

--- README.md
+++ README.md
@@ -4,3 +4,29 @@ git clone https://github.com/Kolya142/nabs_pp.git
 python3 make.py build
 python3 make.py install
 ```
+
+# documentation:
+simple macros:
+```
+i32 - int
+i... - ...
+u32 - uint
+u... - ...
+u8 - char
+str - u8*
+alloc(T, S) - (T*)malloc(typeof(T)*S)
+pbegin, pend, ppbegin - nabs profiler, requires #!use:nabsp
+```
+comple macros:
+```
+$!main - int main()
+$!mainA - int main(int argc, str* argv)
+$!for(T;V;S..E) - for (T V = S; V < E; v++)
+$!forr(count) - for (i32 _ = 0; _ < count; _++) [_ is random variable name]
+#!use:<io,std,ss,ss+,uni,io+,vec,nabsf,nabsp,shash>
+```
+features:
+```
+automatic commma: "i32 i = 0" -> "i32 i = 0;"
+automatic (): "if s == 3 {" -> "if (s == 3) {"
+```
--- make.py
+++ make.py
@@ -1,17 +1,37 @@
 import os
 import sys
 if len(sys.argv) != 2:
-    print("Usage: make.py <build,test,install>")
+    print("Usage: make.py <build,test,install,build-all>")
     exit(1)
 if sys.argv[1] == "build":
     os.system("go build src/*")
+if sys.argv[1] == "build-all":
+    if not os.path.exists("build"):
+        os.mkdir("build")
+    oses = {
+        "windows": ["amd64", "386"],
+        "darwin": ["amd64"],
+        "linux": ["amd64", "arm", "386"]
+    }
+    extentions = {
+        "windows": ".exe",
+        "darwin": ".mac",
+        "linux": ".elf"
+    }
+    for OS in oses:
+        for ARCH in oses[OS]:
+            os.system(f"GOOS={OS} GOARCH={ARCH} go build src/main.go src/compiler.go")
+            if OS == "windows":
+                os.rename("main.exe", f"build/jpp_{OS}-{ARCH}{extentions[OS]}")
+            else:
+                os.rename("main", f"build/jpp_{OS}-{ARCH}{extentions[OS]}")
 if sys.argv[1] == "test":
     os.system("python3 test.py")
 
 if sys.argv[1] == "install":
     if not os.path.exists("/bin/jpp"):
-        os.system("sudo cp compiler /bin/jpp")
+        os.system("sudo cp build/jpp_linux-amd64.elf /bin/jpp")
         os.system(f"sudo chown -c {os.getuid()} /bin/jpp")
     else:
-        os.system("cp compiler /bin/jpp")
+        os.system("cp build/jpp_linux-amd64.elf /bin/jpp")
     print("pp writed to /bin/jpp")
